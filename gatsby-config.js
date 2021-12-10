d34c56e275799dff2b579a33e201d2a684ee8128
Angelo Yang <angelo.r.yang@gmail.com>
2021-12-10 13:50:06 -0600

    Minor Changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,75 +1,67 @@
 module.exports = {
+	
   siteMetadata: {
     // Site URL for when it goes live
-    siteUrl: `https://elastic-meninsky-aa7c74.netlify.app/`,
+    siteUrl: `https://ryang.dev`,
     // Your Name
-    name: 'Ryan Fitzgerald',
+    name: 'Rengang (Angelo) Yang',
     // Main Site Title
-    title: `Ryan Fitzgerald | Full-Stack Developer`,
+    title: `Angelo Yang | Full-Stack Developer`,
     // Description that goes under your name in main bio
-    description: `Lorem ipsum dolor sit amet consectetur adipisicing elit, ipsum.`,
+    description: ``,
     // Optional: Twitter account handle
-    author: `@rfitzio`,
+    author: `@OnlyOneByte`,
     // Optional: Github account URL
-    github: `https://github.com/ryanfitzgerald`,
+    github: `https://github.com/OnlyOneByte`,
     // Optional: LinkedIn account URL
-    linkedin: `https://www.linkedin.com/in/ryanafitzgerald/`,
+    linkedin: `https://www.linkedin.com/in/ryang42/`,
     // Content of the About Me section
-    about: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus perferendis porro cumque ea error ab voluptatem. Temporibus adipisci exercitationem similique itaque quibusdam laudantium, qui molestiae quas, aut amet animi id.`,
+    about: `Ni-Howdy! I'm Angelo. I'm a hardcore minecraft enthusiast, and love collecting single board computers. You can find me volunteering at any hackathon in the central texas area.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
-    projects: [
-      {
-        name: 'Devfolio',
-        description:
-          'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-      {
-        name: 'ChromeExtensionKit',
-        description:
-          'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
-        link: 'https://chromeextensionkit.com/?ref=devfolio',
-      },
-      {
-        name: 'Another Cool Project',
-        description:
-          'Lorem ipsum dolor sit amet consectetur adipisicing elit ducimus perferendis',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
-    ],
+    // projects: [
+    //   {
+    //     name: 'Devfolio',
+    //     description:
+    //       'A zero-config and blazing fast personal site + blog built with GatsbyJs and TailwindCSS',
+    //     link: 'https://github.com/RyanFitzgerald/devfolio',
+    //   },
+    //   {
+    //     name: 'ChromeExtensionKit',
+    //     description:
+    //       'Kit to jump-start your Chrome extension projects with a variety of battle-tested starter templates',
+    //     link: 'https://chromeextensionkit.com/?ref=devfolio',
+    //   },
+      
+    // ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
       {
-        name: 'Acme Corp',
-        description: 'Full-Stack Developer, February 2020 - Present',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'PIMCO',
+        description: 'Full-Stack Developer, June 2021 - Present (FT)',
+        link: 'https://www.pimco.com/en-us/?showSplash=1',
       },
       {
-        name: 'Globex Corp',
-        description: 'Full-Stack Developer, December 2017 - February 2020',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
+        name: 'Research Assistant @ Texas A&M ',
+        description: 'Full-Stack Developer, December 2018 - February 2021',
+        link: '',
       },
-      {
-        name: 'Hooli',
-        description: 'Full-Stack Developer, May 2015 - December 2017',
-        link: 'https://github.com/RyanFitzgerald/devfolio',
-      },
+      
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
         name: 'Languages & Frameworks',
         description:
-          'JavaScript (ES6+), Golang, Node.js, Express.js, React, Ruby on Rails, PHP',
+          'Angular w/ Typescript, Python & Python Tools (Jupyter, Pandas, Tensorflow, Keras), Kotlin and Java (Spring Boot), C++',
       },
       {
         name: 'Databases',
-        description: 'MongoDB, PostreSQL, MySQL',
+        description: 'PostreSQL, MySQL',
       },
       {
         name: 'Other',
         description:
-          'Docker, Amazon Web Services (AWS), CI / CD, Microservices, API design, Agile / Scrum',
+          'Docker, CI / CD w/ Github Actions and Gitlabs, Microservices, API design, Agile / Scrum, AWS Certified Developer',
       },
     ],
   },
